6f9c2ca92d9c577b9ba38bc2eac8f1e9e4b2a9ac
guox191 <guoxiang191@gmail.com>
2018-01-27 17:44:56 +0800

    bin usage

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,48 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
+const path = require('path');
 const request = require('request');
 const eachSeries = require('async/eachSeries');
-const config = require('../example/resources.json');
+const inputPath = process.argv[2];
+
+if (!inputPath) {
+  console.error('Please declare your config file path. \nFor example, `combo-script ./combo.json`');
+  process.exit(1);
+}
+
+const absPath = path.resolve(inputPath);
+const workspace = path.join(absPath, '..');
+let config
+
+try {
+  config = require(absPath);
+  if (!config.resources || !Array.isArray(config.resources)) throw new Error();
+} catch (err) {
+  console.error(`Invalid file '${inputPath}'`);
+  process.exit(1);
+}
+
 const distName = config.dist || 'dist.js';
 let counter = 0;
 
 eachSeries(config.resources, (item, cb) => {
-  if (!item.url) return cb();
+  if (!item.url && !item.path) {
+    console.warn(`Unresolved: ${JSON.stringify(item)}`);
+    return cb();
+  }
 
-  console.log(`Resolving ${item.url}...`);
+  console.log(`Resolving ${item.url || item.path}...`);
   counter += 1;
 
+  if (item.url) download(item, cb);
+  else copy(item, cb);
+
+}, err => {
+  if (err) throw err;
+});
+
+function download(item, cb) {
   const fstream = fs.createWriteStream(distName, { flags: counter > 1 ? 'a' : 'w' });
 
   if (counter > 1) {
@@ -24,6 +56,8 @@ eachSeries(config.resources, (item, cb) => {
     })
     .pipe(fstream)
     .on('finish', cb);
-}, err => {
-  if (err) throw err;
-});
+}
+
+function copy(item, cp) {
+  console.log(1);
+}
