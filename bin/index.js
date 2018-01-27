bda9cc74d58f3029de7c8d60678f4308a619fa49
guox191 <guoxiang191@gmail.com>
2018-01-27 17:09:39 +0800

    http request

--- bin/index.js
+++ bin/index.js
@@ -1,25 +1,29 @@
 const fs = require('fs');
-const axios = require('axios');
+const request = require('request');
 const eachSeries = require('async/eachSeries');
 const config = require('../example/resources.json');
-const distName = config.dist || 'dist.js'
+const distName = config.dist || 'dist.js';
+let counter = 0;
 
 eachSeries(config.resources, (item, cb) => {
   if (!item.url) return cb();
 
-  axios({
-    method: 'get',
-    url: item.url,
-  })
-    .then(res => {
-      fs.writeFileSync(distName, res.data + '\n', { flag: 'a' });
-      cb();
-    })
-    .catch(err => {
-      cb(err);
-    });
-}, err => {
-  if (err) {
-    console.log(err)
+  console.log(`Resolving ${item.url}...`);
+  counter += 1;
+
+  const fstream = fs.createWriteStream(distName, { flags: counter > 1 ? 'a' : 'w' });
+
+  if (counter > 1) {
+    fstream.write('\n');
   }
-})
+
+  request
+    .get(item.url)
+    .on('error', err => {
+      throw err;
+    })
+    .pipe(fstream)
+    .on('finish', cb);
+}, err => {
+  if (err) throw err;
+});
