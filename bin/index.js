d24a92c6fae394207d67db50b1b832c9a6ddd8d4
guox191 <guoxiang191@gmail.com>
2018-01-27 18:10:04 +0800

    copy file

--- bin/index.js
+++ bin/index.js
@@ -7,13 +7,14 @@ const eachSeries = require('async/eachSeries');
 const inputPath = process.argv[2];
 
 if (!inputPath) {
-  console.error('Please declare your config file path. \nFor example, `combo-script ./combo.json`');
+  console.error('Please declare your config file path. \n\
+For example, `combo-script ./combo.json`');
   process.exit(1);
 }
 
 const absPath = path.resolve(inputPath);
 const workspace = path.join(absPath, '..');
-let config
+let config;
 
 try {
   config = require(absPath);
@@ -35,29 +36,36 @@ eachSeries(config.resources, (item, cb) => {
   console.log(`Resolving ${item.url || item.path}...`);
   counter += 1;
 
-  if (item.url) download(item, cb);
-  else copy(item, cb);
+  const fstream = fs.createWriteStream(resolve(distName), { flags: counter > 1 ? 'a' : 'w' });
+  if (counter > 1) fstream.write('\n');
+
+  if (item.url) download(item, fstream, cb);
+  else copy(item, fstream, cb);
 
 }, err => {
   if (err) throw err;
 });
 
-function download(item, cb) {
-  const fstream = fs.createWriteStream(distName, { flags: counter > 1 ? 'a' : 'w' });
-
-  if (counter > 1) {
-    fstream.write('\n');
-  }
-
-  request
-    .get(item.url)
-    .on('error', err => {
-      throw err;
-    })
-    .pipe(fstream)
-    .on('finish', cb);
+function resolve(fpath) {
+  return path.isAbsolute(fpath) ? fpath : path.resolve(workspace, fpath);
 }
 
-function copy(item, cp) {
-  console.log(1);
+function download(item, writeStream, cb) {
+  request
+    .get(item.url)
+    .pipe(writeStream)
+    .on('finish', cb)
+    .on('error', err => {
+      throw err;
+    });
+
+}
+
+function copy(item, writeStream, cb) {
+  fs.createReadStream(resolve(item.path))
+    .pipe(writeStream)
+    .on('finish', cb)
+    .on('error', err => {
+      throw err;
+    });
 }
